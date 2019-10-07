5a938ae5e671e58515cbddeed347e4032b43f03f
Andrei IANAU <aianau@bitdefender.com>
2019-10-07 13:04:51 +0300

    + nonhuman products. men products.

--- defines.py
+++ defines.py
@@ -2,5 +2,33 @@ import datetime
 
 url = 'https://www.decathlon.ro/C-177431-final-de-colectie'
 tor_path = r'D:\programs\TOR\Browser\TorBrowser\Tor\tor.exe'
+proxi = "socks5://localhost:9050"
+
+keywords_men = [
+    'adulţi',
+    'adulți',
+    'bărbați',
+    'bărbaţi',
+    'barbati',
+    'băieți',
+    'baieti'
+]
+
+keywords_human = [
+    'adulţi',
+    'adulți',
+    'adulti',
+    'copii',
+    'damă',
+    'dama',
+    'fete',
+    'bărbaţi',
+    'bărbați',
+    'barbati',
+    'băieţi',
+    'băieți',
+    'baieti'
+]
+
 number_pages = 40
 min_date = datetime.datetime(2000, 1, 1)
